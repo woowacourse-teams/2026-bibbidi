d91d993f6c2167a04c30009f3940312281b80072
Sumin <new.sumyang@gmail.com>
2026-08-24 21:53:07 +0900

    test: H2 데이터소스 검증을 단순화

--- BE/src/test/java/com/bibbidi/wedding/WeddingApplicationTests.java
+++ BE/src/test/java/com/bibbidi/wedding/WeddingApplicationTests.java
@@ -1,12 +1,6 @@
 package com.bibbidi.wedding;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.sql.SQLException;
-import javax.sql.DataSource;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -14,18 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class WeddingApplicationTests {
 
-	@Autowired
-	private DataSource dataSource;
-
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void usesH2DataSource() throws SQLException {
-		try (var connection = dataSource.getConnection()) {
-			assertEquals("H2", connection.getMetaData().getDatabaseProductName());
-		}
 	}
 
 }
