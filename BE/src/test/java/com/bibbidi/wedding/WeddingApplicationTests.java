410c47b9891df30a8fab0207be5f7197b773043c
Sumin <new.sumyang@gmail.com>
2026-08-24 21:19:00 +0900

    test: H2 데이터소스 연결을 검증

--- BE/src/test/java/com/bibbidi/wedding/WeddingApplicationTests.java
+++ BE/src/test/java/com/bibbidi/wedding/WeddingApplicationTests.java
@@ -1,13 +1,31 @@
 package com.bibbidi.wedding;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WeddingApplicationTests {
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void usesH2DataSource() throws SQLException {
+		try (var connection = dataSource.getConnection()) {
+			assertEquals("H2", connection.getMetaData().getDatabaseProductName());
+		}
 	}
 
 }
