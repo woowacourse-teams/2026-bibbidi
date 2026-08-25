9912a1cacaa273f6653a292aa8b5c16a59baaf87
Sumin <new.sumyang@gmail.com>
2026-08-25 09:05:55 +0900

    refactor: 예외 메시지 수정

--- BE/src/main/java/com/bibbidi/wedding/user/exception/DuplicateNicknameException.java
+++ BE/src/main/java/com/bibbidi/wedding/user/exception/DuplicateNicknameException.java
@@ -6,6 +6,6 @@ import com.bibbidi.wedding.common.exception.ProblemType;
 public final class DuplicateNicknameException extends BusinessException {
 
     public DuplicateNicknameException() {
-        super(ProblemType.CONFLICT, "닉네임 중복으로 회원가입에 실패했습니다..");
+        super(ProblemType.CONFLICT, "닉네임 중복으로 회원가입에 실패했습니다.");
     }
 }
