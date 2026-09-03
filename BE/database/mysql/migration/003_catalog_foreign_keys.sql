3cf9bec2c4c4dd2f2c3da6c9069c22efab2ede23
JohnPrk <uo3641493@gmail.com>
2026-09-03 09:14:51 +0900

    chore : 마이그레이션 삭제

--- BE/database/mysql/migration/003_catalog_foreign_keys.sql
+++
@@ -1,9 +0,0 @@
-USE bibbidi;
-
-ALTER TABLE steps
-    ADD CONSTRAINT fk_steps_category
-        FOREIGN KEY (category_id) REFERENCES categories (id);
-
-ALTER TABLE catalog_items
-    ADD CONSTRAINT fk_catalog_items_step
-        FOREIGN KEY (step_id) REFERENCES steps (id);
